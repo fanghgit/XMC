809eaf7645e7e51940b2875f8b0407366b7594cc
fanghgit <hgfang@ucdavis.edu>
2018-03-12 13:57:14 -0700

    update

--- linesearch_clean/linear.cpp
+++ linesearch_clean/linear.cpp
@@ -1481,7 +1481,7 @@ void save_w(FILE *fp, int nr_class, int n, const struct model *model_)
     {
       fprintf(fp, "%d", w[i][j].first);
       fprintf(fp, ":");
-      fprintf(fp, "%lf ", w[i][j].second);
+      fprintf(fp, "%.4lf ", w[i][j].second);
     }
     fprintf(fp, "\n");
   }
